cfb3fcd90b5abc536762b744c8c953749a070433
Alex Shepard <alex@meatfreezer.com>
2021-07-05 22:00:55 -0700

    Initial Commit

---
+++ GenerativeArt/GenerativeArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerativeArtApp.swift
+//  GenerativeArt
+//
+//  Created by Alex Shepard on 7/5/21.
+//
+
+import SwiftUI
+
+@main
+struct GenerativeArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
