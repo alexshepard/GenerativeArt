6c691748c45ada3f0c361f5d3a54c2708ad491fe
Alex Shepard <alex@meatfreezer.com>
2021-07-11 19:55:36 -0700

    move clamp() on cgfloat into an extension

---
+++ GenerativeArt/Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  Extensions.swift
+//  GenerativeArt
+//
+//  Created by Alex Shepard on 7/11/21.
+//
+
+import Foundation
+import CoreGraphics
+
+extension CGFloat {
+    func clamp(range: ClosedRange<CGFloat> = CGFloat(0)...CGFloat(1.0)) -> CGFloat {
+        var newVal = self
+        if newVal > range.upperBound {
+            newVal = range.upperBound
+        } else if newVal < range.lowerBound {
+            newVal = range.lowerBound
+        }
+        return newVal
+    }
+}
