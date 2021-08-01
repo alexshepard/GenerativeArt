706aaa0c650941a7e422b83c112e7ea81c172f6b
Alex Shepard <alex@meatfreezer.com>
2021-07-31 20:25:28 -0700

    remove commented out code

--- GenerativeArt/Generative Design Book/Shape/DiamondModules.swift
+++ GenerativeArt/Generative Design Book/Shape/DiamondModules.swift
@@ -28,44 +28,7 @@ extension Path {
             move(to: rightOrigin)
             addLine(to: CGPoint(x: x, y: y))
         }
-
     }
-    /*
-    init(radiatingLinesIn rect: CGRect, lineCountPerSide: Int = 10) {
-        assert(lineCountPerSide >= 1, "line count per side must be at least 1")
-        
-        self.init()
-        let origin = CGPoint(x: rect.midX, y: rect.midY)
-        
-        for side in 0..<4 {
-            var x2: CGFloat = .zero
-            var y2: CGFloat = .zero
-            
-            for i in 0..<lineCountPerSide {
-                if side == 0 {
-                    // top side - left to right
-                    x2 = rect.minX + (rect.size.width / CGFloat(lineCountPerSide)) * CGFloat(i)
-                    y2 = rect.minY
-                } else if side == 1 {
-                    // left side, bottom to top
-                    x2 = rect.minX
-                    y2 = rect.maxY - (rect.size.height / CGFloat(lineCountPerSide)) * CGFloat(i)
-                } else if side == 2 {
-                    // right side, top to bottom
-                    x2 = rect.maxX
-                    y2 = rect.minY + (rect.size.height / CGFloat(lineCountPerSide)) * CGFloat(i)
-                } else if side == 3 {
-                    // bottom side, right to left
-                    x2 = rect.maxX - (rect.size.width / CGFloat(lineCountPerSide)) * CGFloat(i)
-                    y2 = rect.maxY
-                }
-                let end = CGPoint(x: x2, y: y2)
-                move(to: origin)
-                addLine(to: end)
-            }
-        }
-    }
-     */
 }
 
 struct DiamondModules: View, Sketch {
